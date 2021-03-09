fcc5849d56ebfe0944f894888daa484d9f3700b9
Khuat <khuatgia@noyce.cs.grinnell.edu>
2021-03-09 09:43:59 -0600

    Tuesday 9/3

--- client.c
+++ client.c
@@ -7,50 +7,93 @@
 
 #define BUFFER_LEN 256
 
-int main(int argc, char** argv) {
-  if (argc != 3) {
+int main(int argc, char **argv)
+{
+  if (argc != 3)
+  {
     fprintf(stderr, "Usage: %s <server name> <port>\n", argv[0]);
     exit(1);
   }
 
   // Read command line arguments
-  char* server_name = argv[1];
+  char *server_name = argv[1];
   unsigned short port = atoi(argv[2]);
 
   // Connect to the server
   int socket_fd = socket_connect(server_name, port);
-  if (socket_fd == -1) {
+  if (socket_fd == -1)
+  {
     perror("Failed to connect");
     exit(2);
   }
 
   // Set up file streams
-  FILE* to_server = fdopen(dup(socket_fd), "wb");
-  if (to_server == NULL) {
+  // when the client looks like to
+  FILE *to_server = fdopen(dup(socket_fd), "wb");
+  if (to_server == NULL)
+  {
     perror("Failed to open stream to server");
     exit(2);
   }
 
-  FILE* from_server = fdopen(dup(socket_fd), "rb");
-  if (from_server == NULL) {
+  FILE *from_server = fdopen(dup(socket_fd), "rb");
+  if (from_server == NULL)
+  {
     perror("Failed to open stream from server");
     exit(2);
   }
 
+  /*
+  client: 
+  Declare a message variable string
+  While (message != 'quit") {
+    // Write to the server 
+    fprintf(..., message);
+    // wait for server response 
+    // server responds, print out
+    
+  }
+
+  server: 
+  while (message != quit) {
+    keep sending back what you receive 
+  }
+  */
   // Read a message from the server
-  char buffer[BUFFER_LEN];
-  if (fgets(buffer, BUFFER_LEN, from_server) == NULL) {
+  char buffer[BUFFER_LEN] = "";
+  /*
+  if (fgets(buffer, BUFFER_LEN, from_server) == NULL)
+  {
     perror("Reading from server failed");
     exit(2);
   }
+  */
+  char line[256];
+  while (fgets(line, sizeof line, stdin) != NULL)
+  {
+    if (strcmp(line, "quit\n") == 0)
+    {
+      fprintf(to_server, line);
+      fflush(to_server);
+      break;
+    }
+    // Send it to the server
+    fprintf(to_server, line);
+    fflush(to_server);
+    // Reading what is from the server
+    fgets(buffer, BUFFER_LEN, from_server);
+    // Print the message sent back from server
+    printf("Server: %s", buffer);
+  }
 
-  printf("Server sent: %s\n", buffer);
+  //printf("Server sent: %s\n", buffer);
 
   // Send a message to the server
-  fprintf(to_server, "Hello server!\n");
+  // it looks like the
+  //fprintf(to_server, "Hello server!\n");
 
   // Flush the output buffer
-  fflush(to_server);
+  //fflush(to_server);
 
   // Close file streams
   fclose(to_server);
--- server.c
+++ server.c
@@ -7,17 +7,20 @@
 
 #define BUFFER_LEN 256
 
-int main() {
+int main()
+{
   // Open a server socket
   unsigned short port = 0;
   int server_socket_fd = server_socket_open(&port);
-  if (server_socket_fd == -1) {
+  if (server_socket_fd == -1)
+  {
     perror("Server socket was not opened");
     exit(2);
   }
 
   // Start listening for connections, with a maximum of one queued connection
-  if (listen(server_socket_fd, 1)) {
+  if (listen(server_socket_fd, 1))
+  {
     perror("listen failed");
     exit(2);
   }
@@ -26,7 +29,8 @@ int main() {
 
   // Wait for a client to connect
   int client_socket_fd = server_socket_accept(server_socket_fd);
-  if (client_socket_fd == -1) {
+  if (client_socket_fd == -1)
+  {
     perror("accept failed");
     exit(2);
   }
@@ -34,33 +38,52 @@ int main() {
   printf("Client connected!\n");
 
   // Set up file streams to access the socket
-  FILE* to_client = fdopen(dup(client_socket_fd), "wb");
-  if (to_client == NULL) {
+  FILE *to_client = fdopen(dup(client_socket_fd), "wb");
+  if (to_client == NULL)
+  {
     perror("Failed to open stream to client");
     exit(2);
   }
 
-  FILE* from_client = fdopen(dup(client_socket_fd), "rb");
-  if (from_client == NULL) {
+  FILE *from_client = fdopen(dup(client_socket_fd), "rb");
+  if (from_client == NULL)
+  {
     perror("Failed to open stream from client");
     exit(2);
   }
 
+  char buffer[BUFFER_LEN];
+
+  while (fgets(buffer, BUFFER_LEN, from_client) != NULL)
+  {
+    int len = strlen(buffer);
+    printf("len = %d\n", len);
+    for (int i = 0; i < len; i++)
+    {
+      buffer[i] = toupper(buffer[i]);
+    }
+    fprintf(to_client, buffer);
+    fflush(to_client);
+    printf("Server: %s", buffer);
+  }
+
   // Send a message to the client
-  fprintf(to_client, "Hello client!\n");
+  //fprintf(to_client, "Hello client!\n");
 
   // Flush the output buffer
-  fflush(to_client);
+  //fflush(to_client);
 
   // Receive a message from the client
+  /*
   char buffer[BUFFER_LEN];
-  if (fgets(buffer, BUFFER_LEN, from_client) == NULL) {
+  if (fgets(buffer, BUFFER_LEN, from_client) == NULL)
+  {
     perror("Reading from client failed");
     exit(2);
   }
-
+  
   printf("Client sent: %s\n", buffer);
-
+  */
   // Close file streams
   fclose(to_client);
   fclose(from_client);
